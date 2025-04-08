12200c352a6bf5a959df9b91a86458027ee0c306
fullth <lth129@naver.com>
2025-04-08 21:56:11 +0900

    feat: 프로젝트 초기설정

---
+++ src/main/kotlin/com/fullth/kotlin_bootstrap/KotlinBootstrapApplication.kt
@@ -0,0 +1,11 @@
+package com.fullth.kotlin_bootstrap
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class KotlinBootstrapApplication
+
+fun main(args: Array<String>) {
+	runApplication<KotlinBootstrapApplication>(*args)
+}
---
+++ src/test/kotlin/com/fullth/kotlin_bootstrap/KotlinBootstrapApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.fullth.kotlin_bootstrap
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class KotlinBootstrapApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
